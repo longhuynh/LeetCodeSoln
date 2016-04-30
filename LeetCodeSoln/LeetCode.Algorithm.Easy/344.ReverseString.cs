62f5c4a7dcca0004964e7e4962c70e1184b7493d
Long Huynh <huynhquanglong@gmail.com>
2016-04-30 17:33:12 -0500

    Reverse String

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/344.ReverseString.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * https://leetcode.com/problems/reverse-string/
+ * Write a function that takes a string as input and returns the string reversed.
+ * 
+ * Tags: String
+ */
+
+
+using System;
+
+namespace LeetCode.Algorithm.Easy
+{
+    class ReverseString
+    {
+        public string ReverseAString(string s)
+        {
+            //char[] charArray = s.ToCharArray();
+            //int length = s.Length;
+            //int halfLength = length / 2;
+
+            //for(int i = 0; i < halfLength; i++ )
+            //{
+            //    char temp = charArray[i];
+            //    charArray[i] = charArray[length - 1 - i];
+            //    charArray[length - 1 - i] = temp;
+            //}
+            // new string(charArray);
+            // if (s.Length == 0) 
+            //     return s;
+
+            //return ReverseString(s.Substring(1)) + s[0];
+
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
