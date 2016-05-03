d05f32682ec8e6f49d6323b7be8baf4596f3beb7
Long Huynh <huynhquanglong@gmail.com>
2016-05-03 17:34:52 -0500

    roman to int and int to roman

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/013.RomanToInteger.cs
@@ -0,0 +1,54 @@
+﻿/**
+ * https://leetcode.com/problems/roman-to-integer/
+ * 
+ * Tags: Brainteaser
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithm.Easy
+{
+    public class RomanToInteger
+    {
+        public static int RomanToInt(string s)
+        {
+            int toInt = 0;
+            if (string.IsNullOrEmpty(s)) return toInt;
+
+            char[] charArray = s.ToCharArray();
+            int length = charArray.Length - 1;
+
+            for (int i = length; i >= 0; i--)
+            {
+                switch (charArray[i])
+                {
+                    case 'M':
+                        toInt += 1000;
+                        break;
+                    case 'D':
+                        toInt += 500;
+                        break;
+                    case 'C':
+                        toInt += 100*(toInt >= 500 ? -1 : 1); 
+                        break;
+                    case 'L':
+                        toInt += 50;
+                        break;
+                    case 'X':
+                        toInt += 10*(toInt >= 50 ? -1 : 1); 
+                        break;
+                    case 'V':
+                        toInt += 5;
+                        break;
+                    case 'I':
+                        toInt += (toInt >= 5 ? -1 : 1); 
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return toInt;
+        }
+    }
+}
---
+++ LeetCodeSoln/LeetCode.Algorithm.Medium/012.IntegerToRoman.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * https://leetcode.com/problems/integer-to-roman/
+ * 
+ * Tags: Brainteaser
+ */
+
+using System.Text;
+
+namespace LeetCode.Algorithm.Medium
+{
+    public class IntegerToRoman
+    {
+        private readonly int[] ints = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        private readonly string[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        public string IntToRoman(int number)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 0;
+            while (number > 0 && i < ints.Length)
+            {
+                if (number >= ints[i])
+                {
+                    stringBuilder.Append(romans[i]);
+                    number -= ints[i]; // update number
+                }
+                else
+                {
+                    i++; // move to next roman
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        //public string IntToRoman(int num)
+        //{
+        //    for (int i = 0; i < intDict.length; i++)
+        //    {
+        //        if (intDict[i] <= num)
+        //        {
+        //            return romanDict[i] + intToRoman(num - intDict[i]);
+        //        }
+        //    }
+        //    return ""; // Note the return statement
+        //}
+    }
+}
