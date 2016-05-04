11a4fb697652b86f16092cad4edc6cc7b74a8cd2
Long Huynh <huynhquanglong@gmail.com>
2016-05-03 23:23:13 -0500

    RemoveNodeNthFromEnd

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/019.RemoveNodeNthFromEnd.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * 
+ * Tags: Linkedlist
+ */
+
+namespace LeetCode.Algorithm.Easy
+{
+    class RemoveNodeNthFromEnd
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode listNode = new ListNode(0);
+            listNode.Next = head;
+
+            ListNode node1 = listNode;
+            ListNode node2 = listNode;
+
+            for (int i = 0; i < n; i++)
+            {
+                node2 = node2.Next;
+            }
+
+            while (node2.Next != null)
+            {
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+
+            node1.Next = node1.Next.Next;
+            return listNode.Next;
+        }
+    }
+}
