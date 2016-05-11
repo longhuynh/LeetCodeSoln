2d513855dbc698a085d24582eff40890596c180d
Long Huynh <huynhquanglong@gmail.com>
2016-05-11 18:22:39 -0500

    172.FactorialTrailingZeroes

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/172.FactorialTrailingZeroes.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * https://leetcode.com/problems/factorial-trailing-zeroes/
+ * 
+ * Tags: Math
+ */
+
+
+using System;
+
+namespace LeetCode.Algorithm.Easy
+{
+    public class FactorialTrailingZeroes
+    {
+        // O(log5-n)
+        public static int TrailingZeroes(int n)
+        {
+            int trailingZeroes = 0;
+            while (n > 0)
+            {
+                n /= 5;
+                trailingZeroes += n; // add # of 5 in n
+            }
+            return trailingZeroes;
+        }
+
+        // Recursive
+        public static int TrailingZeroesB(int n)
+        {
+            return n <= 0 ? 0 : n / 5 + TrailingZeroes(n / 5);
+        }
+    }
+}
