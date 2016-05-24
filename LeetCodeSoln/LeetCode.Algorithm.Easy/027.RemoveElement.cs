b7fe238ba98714f793c4653e8d9410c814e7d2fb
Long Huynh <huynhquanglong@gmail.com>
2016-05-23 21:05:38 -0500

    Remove Element

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/027.RemoveElement.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * https://leetcode.com/problems/remove-element/
+ * 
+ * Tags: Array, Two Pointer
+ */
+
+namespace LeetCode.Algorithm.Easy
+{
+    public class RemoveElementSoln
+    {
+        public int RemoveElement(int[] items, int element)
+        {
+            if (items == null || items.Length == 0) return 0;
+            int i = 0;
+            int j = items.Length;
+            while (i < j)
+            {
+                if (items[i] == element)
+                {
+                    items[i] = items[j - 1]; // move last element
+                    j--; // decrease Length
+                }
+                else i++; // move on
+            }
+            return j;
+        }
+    }
+}
