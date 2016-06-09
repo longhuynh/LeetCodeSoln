1ff416e62000c59391ab87cda75afafbe15309cb
Long Huynh <huynhquanglong@gmail.com>
2016-06-08 21:26:17 -0500

    Reverse Words in a String

---
+++ LeetCodeSoln/LeetCode.Algorithm.Medium/151.ReverseWordsinaString.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * https://leetcode.com/problems/reverse-words-in-a-string/
+ * 
+ * Tags: String
+ */
+
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Algorithm.Medium
+{
+    public class ReverseWord
+    {
+        public string ReverseWords(string sentence)
+        {
+            sentence = sentence.Trim();
+            var left = sentence.Length - 1;
+            var right = sentence.Length - 1;
+            var stringBuilder = new StringBuilder();
+            while (right >= 0)
+            {
+                if (sentence[right] == ' ')
+                {
+                    stringBuilder.Append(sentence.Substring(right + 1, left - right));
+                    stringBuilder.Append(" ");
+                    while (sentence[right] == ' ')
+                    {
+                        right--;
+                    }
+                    left = right;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            stringBuilder.Append(sentence.Substring(right + 1, left - right));
+            return stringBuilder.ToString();
+        }
+    }
+}
