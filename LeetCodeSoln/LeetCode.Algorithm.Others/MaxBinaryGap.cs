66a1b4b06242884f293c869ae4e63c032d14b908
Long Huynh <huynhquanglong@gmail.com>
2016-06-22 08:00:38 -0500

    Max binary gap

---
+++ LeetCodeSoln/LeetCode.Algorithm.Others/MaxBinaryGap.cs
@@ -0,0 +1,35 @@
+﻿//
+// For example, 9's binary form is 1001, the gap is 2.
+//
+
+namespace LeetCode.Algorithm.Others
+{
+    public class MaxBinaryGap
+    {
+        public static int GetGap(int number)
+        {
+            var max = 0;
+            var count = -1;
+
+            while (number > 0)
+            {
+                // Get right most bit & shift right
+                var r = number & 1;
+                number = number >> 1;
+
+                if (0 == r && count >= 0)
+                {
+                    count++;
+                }
+
+                if (1 == r)
+                {
+                    max = count > max ? count : max;
+                    count = 0;
+                }
+            }
+
+            return max;
+        }
+    }
+}
