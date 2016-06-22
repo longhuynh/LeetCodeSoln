611528e5d375c996fc611a4ff07d0d566220d878
Long Huynh <huynhquanglong@gmail.com>
2016-06-21 22:23:49 -0500

    Single Number

---
+++ LeetCodeSoln/LeetCode.Algorithm.Medium/136.SingleNumber.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * https://leetcode.com/problems/single-number/
+ * 
+ * Tags: Bit Manipulation
+
+ */
+
+using System;
+
+namespace LeetCode.Algorithm.Medium
+{
+    public class SingleNumber
+    {
+        public int FindSingleNumber(int[] items)
+        {
+            var x = 0;
+            foreach (var i in items)
+            {
+                x = x ^ i;
+            }
+            return x;
+        }
+    }
+}
