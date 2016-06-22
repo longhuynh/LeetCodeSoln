6c62580d9aa5e4edbeb41f6805cf770104ad3602
Long Huynh <huynhquanglong@gmail.com>
2016-06-22 08:03:59 -0500

    NumberOfOneBit

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/191.NumberOfOneBit.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * https://leetcode.com/problems/number-of-1-bits/
+ * 
+ * Tags: Bit Manipulation
+
+ */
+
+
+using System;
+
+namespace LeetCode.Algorithm.Easy
+{
+    public class NumberOfOneBit
+    {
+        public int HammingWeight(uint number)
+        {
+            var count =0;
+
+            while (number > 0)
+            {
+                var r = number & 1;
+                number = number >> 1;
+
+                if (1 == r)
+                {
+                    count ++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
