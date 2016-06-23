7def8701059f7ae2f8430aba648145a487bbfdca
Long Huynh <huynhquanglong@gmail.com>
2016-06-23 09:02:34 -0500

    TwoSum

---
+++ LeetCodeSoln/LeetCode.Algorithm.Easy/001.TwoSum.cs
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/two-sum/
+ * 
+ * Tags: String
+ **/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithm.Easy
+{
+    public class TwoSumSolutionn
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            var result = new int[2];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (dictionary.ContainsKey(numbers[i]))
+                {
+                    var index = dictionary[numbers[i]];
+                    result[0] = index;
+                    result[1] = i;
+                    break;
+                }
+                if (!dictionary.ContainsKey(target - numbers[i]))
+                {
+                    dictionary.Add(target - numbers[i], i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
